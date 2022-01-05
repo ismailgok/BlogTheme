439b9a4a85fa8d7af2e50269d3f9d2d5f7724e15
İsmail <ismailsilici@gmail.com>
2022-01-06 00:20:12 +0300

    Html CSS blog theme
    Html CSS blog theme

---
+++ app.js
@@ -0,0 +1,50 @@
+let left = document.querySelector(".leftSide")
+let right = document.querySelector(".rightSide")
+let container = document.querySelector(".container")
+let btn = document.querySelector(".themeBtn")
+let buton = document.querySelector(".btn1")
+const renk1 = "#5FCB71"
+const renk2 = "#D9FCF4"
+const renk3 = "#4f4f4f"
+const renk4 = "#4a4a4a"
+
+
+// btn.addEventListener("click", function(e) {
+//     left.style.transition = "all 0.5s ease"
+//     right.style.transition = "all 0.5s ease"
+
+//     // left.style.backgroundColor = "#4f4f4f"
+//     // right.style.backgroundColor = "#4a4a4a"
+//     if (left.style.backgroundColor = renk1) {
+//         left.style.backgroundColor = renk3
+//         right.style.backgroundColor = renk4
+//     }
+//     if (right.style.backgroundColor = renk4) {
+//         left.style.backgroundColor = renk1
+//         right.style.backgroundColor = renk2
+//         btn.classList = "orj"
+//     }
+
+//     e.preventDefault()
+
+// })
+
+// function getData(e) {
+//     left.style.transition = "all 0.5s ease"
+//     right.style.transition = "all 0.5s ease"
+
+//     left.style.backgroundColor = renk3
+//     right.style.backgroundColor = renk4
+
+//     e.preventDefault()
+// }
+
+function setColor(e) {
+    if (left.style.backgroundColor = renk1) {
+        left.style.backgroundColor = renk3
+        buton.style.color = "red"
+    }
+
+
+
+}
